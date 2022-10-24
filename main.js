bf349254afce359caf026c1ffe2a53444c6a554f
yousef <yousefromeia@yahoo.com>
2022-10-24 22:00:52 +0200

    intial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+let btn = document.getElementById("btn");
+let menu = document.getElementById("menu");
+
+
+function iee(){
+    btn.classList.toggle("kk");
+    menu.classList.toggle("kk");
+
+    }
+btn.onclick=iee
